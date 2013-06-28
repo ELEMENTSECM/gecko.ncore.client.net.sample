b010e46b450c5f0157ebe224122af12e878832b6
Steinar Herland <she@gecko.no>
2013-06-28 13:52:56 +0200

    Add test file for classification

---
+++ EphorteTests/ClassificationTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Gecko.NCore.Client.ObjectModel.V3.En;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+// ReSharper disable InconsistentNaming
+namespace EphorteTests
+{
+    [TestClass]
+    public class ClassificationTest : EphorteUnitTestBase
+    {
+        [TestMethod]
+        public void QueryForClassThatDoesNotExist_ReturnsZeroRecords()
+        {
+            //Norwegian
+            //var query = context.Query<Klassering>().Where(x => x.Ordningsverdi.Tittel == "12345678910").Count();
+            
+            //var query = EphorteContext.Query<Classification>().Where(x => x.Class.Id == "DoesNotExist");
+
+            //This query would give same result:
+            var query = EphorteContext.Query<Classification>().Where(x => x.ClassId == "DoesNotExist");
+
+            var count = query.Count();
+            Assert.AreEqual(0, count);
+        }
+    }
+}
