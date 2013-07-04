3212925593bcb6387ee0c770460f6d0b33ca1803
Steinar Herland <she@gecko.no>
2013-07-04 06:55:35 +0200

    Add test - QueryForClassificationThatIsInUse_DoesNotReturnsZeroRecords

--- EphorteTests/ClassificationTest.cs
+++ EphorteTests/ClassificationTest.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Security.Cryptography;
 using Gecko.NCore.Client.ObjectModel.V3.En;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -21,6 +24,22 @@ namespace EphorteTests
 
             var count = query.Count();
             Assert.AreEqual(0, count);
+        } 
+        
+        [TestMethod]
+        public void QueryForClassificationThatIsInUse_DoesNotReturnsZeroRecords()
+        {
+            var firstClassification = EphorteContext.Query<Classification>().First(c => c.Description != null);
+
+            Console.WriteLine(firstClassification.ClassId);
+
+            var query1 = EphorteContext.Query<Classification>().Where(x => x.ClassId == firstClassification.ClassId);
+
+            Assert.AreNotEqual(0, query1.Count(), "Query 1");
+
+            //WILL Currently fail:
+            //var query2 = EphorteContext.Query<Classification>().Where(x => x.Class.Id == firstClassification.ClassId);
+            //Assert.AreNotEqual(0, query2.Count(), "Query 2");
         }
     }
 }
